4281a678835bac2a5899d1d14a18d8526d3e3aab
Eymenpixel <akguneymen98@gmail.com>
2026-04-15 17:06:44 +0300

    Update Baslat.sh

--- Baslat.sh
+++ Baslat.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -p 8006:8006 --device=/dev/kvm --cap-add NET_ADMIN -e VERSION=tiny11 dockur/windows
+sudo docker run -it --rm -p 8006:8006 --device=/dev/kvm --cap-add NET_ADMIN -e VERSION=11 dockur/windows
